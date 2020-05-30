81e737145d4899bd7664ff8b06d508f6d256eee0
Alejandro F <afuste7@gmail.com>
2020-05-30 19:25:44 -0400

    added components folder with Footer index and style files

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer>
+			<a href="#">
+				<i class="fas fa-arrow-up" />To the top
+			</a>
+
+			<div>Put Social Media Icons Here</div>
+		</footer>
+	);
+};
+
+export default Footer;
