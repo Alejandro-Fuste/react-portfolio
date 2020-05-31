53093ee4277d20060230f9fdba4aafd54a0e7f4a
Alejandro F <afuste7@gmail.com>
2020-05-30 22:05:43 -0400

    added fourth parallax and footer components to app file

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,11 @@ import AboutMe from './components/AboutMe';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
 import Parallax from './components/Parallax';
+import Footer from './components/Footer';
 import Pic1 from './assets/pic1.jpg';
 import Pic2 from './assets/pic2.jpg';
 import Pic3 from './assets/pic3.jpg';
+import Pic4 from './assets/pic4.jpg';
 
 class App extends Component {
 	render() {
@@ -21,6 +23,8 @@ class App extends Component {
 				<Portfolio />
 				<Parallax image={Pic3} />
 				<Contact />
+				<Parallax image={Pic4} />
+				<Footer />
 			</div>
 		);
 	}
