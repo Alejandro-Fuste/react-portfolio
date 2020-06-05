e7c16ac54e8cbae31aed076ff00952c1508f09e6
Alejandro F <afuste7@gmail.com>
2020-06-04 21:37:21 -0400

    added build folder

---
+++ client/build/precache-manifest.f0ee0298bb2dc322b7a5d4b92586097c.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "75ef79c97506e45b1cff",
+    "url": "/static/css/main.7c0f72dd.chunk.css"
+  },
+  {
+    "revision": "75ef79c97506e45b1cff",
+    "url": "/static/js/main.f1c2d267.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9730acd76b53f9495c93",
+    "url": "/static/js/2.837bbd8c.chunk.js"
+  },
+  {
+    "revision": "df3fdbef04c0b6f898bdb3c910c03b8a",
+    "url": "/static/media/AF_logo_3.df3fdbef.svg"
+  },
+  {
+    "revision": "ce3d3900b67fcf24b4be6e81ef0f905f",
+    "url": "/static/media/profilePic.ce3d3900.jpeg"
+  },
+  {
+    "revision": "a5087189e774cd0016b35e390b990932",
+    "url": "/static/media/Poddler.a5087189.png"
+  },
+  {
+    "revision": "77df640d47f1e03e21148bcc2eb1ee25",
+    "url": "/static/media/burger.77df640d.png"
+  },
+  {
+    "revision": "fa7d579e3b74a1586fc97396c6f22306",
+    "url": "/static/media/empTrack1.fa7d579e.png"
+  },
+  {
+    "revision": "319f79b6dd12f4c7a552de60b0b49fc9",
+    "url": "/static/media/templateEngine.319f79b6.png"
+  },
+  {
+    "revision": "d2b59c4e06066eee7497ae839087e037",
+    "url": "/static/media/readme.d2b59c4e.png"
+  },
+  {
+    "revision": "93d4215ffc611ce05787d045ad1d2c4c",
+    "url": "/static/media/projectOne.93d4215f.png"
+  },
+  {
+    "revision": "9ecb299c44e7ddd483c79c1babb83366",
+    "url": "/static/media/weatherDashboard.9ecb299c.png"
+  },
+  {
+    "revision": "8e22f10d57dbc487181b493901847092",
+    "url": "/static/media/scheduler.8e22f10d.png"
+  },
+  {
+    "revision": "49c42f118735cb796ff9c1ad46d5bf77",
+    "url": "/static/media/passwordGenerator.49c42f11.png"
+  },
+  {
+    "revision": "9c5077470f8e5cbf63e86440cc5ccab9",
+    "url": "/static/media/orlando.9c507747.jpg"
+  },
+  {
+    "revision": "20a3c69603c7809ed5e9aae50653847a",
+    "url": "/static/media/pic1.20a3c696.jpg"
+  },
+  {
+    "revision": "9163725335aa07d0e9f1b81103e45c12",
+    "url": "/static/media/pic2.91637253.jpg"
+  },
+  {
+    "revision": "15dd31726448dffc6ebc42bd05653638",
+    "url": "/static/media/pic3.15dd3172.jpg"
+  },
+  {
+    "revision": "a04a0c3d4915aed864aabd3fddb5f6fa",
+    "url": "/static/media/pic4.a04a0c3d.jpg"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "015f47cebc2aeb2e32beaf04009aa555",
+    "url": "/index.html"
+  }
+];
