c70227c5dfb8b94db7f1743b5d2bc42499b5fa7b
Alejandro F <afuste7@gmail.com>
2020-07-23 13:21:32 -0400

    added code for portfolio schema

---
+++ models/portfolio.js
@@ -0,0 +1,33 @@
+// Require mongoose
+const mongoose = require('mongoose');
+
+// Create schema variable
+const Schema = mongoose.Schema;
+
+const portfolioSchema = new Schema({
+	name: {
+		type: String,
+		trim: true,
+		required: 'Enter the name of the stock project'
+	},
+	description: {
+		type: String,
+		trim: true
+	},
+	image: {
+		type: String,
+		trim: true
+	},
+	deployedLink: {
+		type: String,
+		trim: true
+	},
+	repoLink: {
+		type: String,
+		trim: true
+	}
+});
+
+const Portfolio = mongoose.model('Portfolio', portfolioSchema);
+
+module.exports = Portfolio;
