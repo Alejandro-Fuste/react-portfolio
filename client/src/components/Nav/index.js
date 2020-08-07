c2d345ae8d13af0f54f26518ea469956c7ce715a
Alejandro F <afuste7@gmail.com>
2020-08-07 10:59:41 -0400

    changed img margin and nav links sizing

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,59 +1,43 @@
 import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import './style.css';
-// import Navbar from 'react-bootstrap/Navbar';
+
 import Logo from '../../assets/AF_logo_3.svg';
 
-const Nav = () => {
-	return (
-		<header>
-			<div className="container-fluid" style={{ padding: '0px' }}>
-				<div className="row">
-					<div className="col-sm-12" style={{ padding: '0px' }}>
-						<nav
-							className="navbar navbar-expand-md justify-content-end navbar-light"
-							id="navbar"
-							style={{ padding: '8px 0px' }}
-						>
-							<a className="navbar-brand mr-auto" href="#home">
-								<img src={Logo} alt="logo" id="logo" />
-							</a>
-							<button
-								className="navbar-toggler border-0"
-								data-toggle="collapse"
-								data-target="#navbarMenu"
-							>
-								<span className="navbar-toggler-icon" />
-							</button>
-							<div className="collapse navbar-collapse" id="navbarMenu">
-								<ul className="navbar-nav ml-auto">
-									<li className="nav-item">
-										<a className="nav-link active" href="#home">
-											Home
-										</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link active" href="#about">
-											<i className="fas fa-user" />About
-										</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link active" href="#portfolio">
-											<i className="fas fa-th" />Portfolio
-										</a>
-									</li>
-									<li className="nav-item">
-										<a className="nav-link active" href="#contact">
-											<i className="fas fa-envelope" />Contact
-										</a>
-									</li>
-								</ul>
-							</div>
-						</nav>
-					</div>
-				</div>
-			</div>
-		</header>
-	);
-};
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faTh } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-export default Nav;
+function nav() {
+	return (
+		<div>
+			<Navbar bg="light" expand="lg">
+				<Navbar.Brand href="#home">
+					<img src={Logo} alt="logo" id="logo" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto">
+						<Nav.Link href="#home" style={{ color: 'black' }}>
+							<FontAwesomeIcon icon={faHome} className="icon" />Home
+						</Nav.Link>
+						<Nav.Link href="#about" style={{ color: 'black' }}>
+							<FontAwesomeIcon icon={faUser} className="icon" />About
+						</Nav.Link>
+						<Nav.Link href="#portfolio" style={{ color: 'black' }}>
+							<FontAwesomeIcon icon={faTh} className="icon" />Portfolio
+						</Nav.Link>
+						<Nav.Link href="#contact" style={{ color: 'black' }}>
+							<FontAwesomeIcon icon={faEnvelope} className="icon" />Contact
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		</div>
+	);
+}
+
+export default nav;
