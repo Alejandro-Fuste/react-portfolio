0c7b04a2a34f25e279e9f94b725c61595f55746f
Alejandro F <afuste7@gmail.com>
2021-04-09 10:40:43 -0400

    added react-three-fiber in App.js file

--- client/src/App.js
+++ client/src/App.js
@@ -2,29 +2,32 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 
-import Parallax from './components/Parallax';
-import Pic1 from './assets/pic1.jpg';
-import Pic2 from './assets/pic2.jpg';
-import Pic3 from './assets/pic3.jpg';
-import Pic4 from './assets/pic4.jpg';
+// import Parallax from './components/Parallax';
+// import Pic1 from './assets/pic1.jpg';
+// import Pic2 from './assets/pic2.jpg';
+// import Pic3 from './assets/pic3.jpg';
+// import Pic4 from './assets/pic4.jpg';
 
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+// import AboutMe from './components/AboutMe';
+// import Portfolio from './components/Portfolio';
+// import Contact from './components/Contact';
+// import Footer from './components/Footer';
+
+import Homepage from './pages/Homepage';
 
 function App() {
 	return (
 		<React.Fragment>
 			<Nav />
-			<Parallax image={Pic1} />
+			{/* <Homepage /> */}
+			{/* <Parallax image={Pic1} />
 			<AboutMe />
 			<Parallax image={Pic2} />
 			<Portfolio />
 			<Parallax image={Pic3} />
 			<Contact />
 			{/* <Parallax image={Pic4} /> */}
-			<Footer />
+			{/* <Footer /> */}
 		</React.Fragment>
 	);
 }
