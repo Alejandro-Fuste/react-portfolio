0af93f1d26026ae33974fca0ae78c6d59d936e26
Alejandro F <afuste7@gmail.com>
2021-04-19 19:30:34 -0400

    added added mesh, boxBufferGeometry, meshLambertMaterial JSX properties

--- client/src/components/Box/index.js
+++ client/src/components/Box/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
+// import { Canvas } from '@react-three/fiber';
+
+function Box(props) {
+	return (
+		<mesh>
+			<boxBufferGeometry attach="geometry" />
+			<meshLambertMaterial attach="material" color="blue" />
+		</mesh>
+	);
+}
