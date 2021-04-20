7dc7f4f156975df5babd31347815fe169f79c73b
Alejandro F <afuste7@gmail.com>
2021-04-19 20:04:15 -0400

    added mesh ref attritube to mesh

--- client/src/components/Box/index.js
+++ client/src/components/Box/index.js
@@ -2,10 +2,11 @@ import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 
 function Box(props) {
+	const mesh = useRef();
 	useFrame(() => (mesh.current.rotation.x += 0.01));
 
 	return (
-		<mesh>
+		<mesh ref={mesh}>
 			<boxBufferGeometry attach="geometry" />
 			<meshLambertMaterial attach="material" color="blue" />
 		</mesh>
