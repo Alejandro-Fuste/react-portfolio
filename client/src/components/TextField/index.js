fc45b2c30a93d5713784b0e95e730f3f7f020e23
Alejandro F <afuste7@gmail.com>
2021-04-29 19:40:46 -0400

    added className to div

--- client/src/components/TextField/index.js
+++ client/src/components/TextField/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function TextField({ name }) {
   return (
-    <div>
+    <div className="textFieldDiv">
       <h4>
         Hello, I'm <span className="name">{name}.</span>
       </h4>
