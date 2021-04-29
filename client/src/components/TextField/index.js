fc7cd57945eaab810273f5b1f84ad2df08be2077
Alejandro F <afuste7@gmail.com>
2021-04-29 19:38:30 -0400

    added TextField component to Homepage.js file. Also, added period to h4 tag

--- client/src/components/TextField/index.js
+++ client/src/components/TextField/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
-function TextField({name}) {
-    return (
-        <div>
-            <h4>Hello, I'm <span>{name}</span></h4>
-            
-        </div>
-    )
+function TextField({ name }) {
+  return (
+    <div>
+      <h4>
+        Hello, I'm <span className="name">{name}.</span>
+      </h4>
+    </div>
+  );
 }
 
-export default TextField
+export default TextField;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -7,12 +7,15 @@ import Box from "../components/Box";
 
 const Homepage = () => {
   return (
-    <Canvas style={{ height: "60vh", width: "50vw", right: "-50%" }}>
-      <OrbitControls />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Box />
-    </Canvas>
+    <>
+      <TextField name="Alejandro Fuste" />
+      <Canvas style={{ height: "60vh", width: "50vw", right: "-50%" }}>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <Box />
+      </Canvas>
+    </>
   );
 };
 
