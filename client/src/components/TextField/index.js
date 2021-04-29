acc0374509095bef8e7c1809cee26c7d76a6965a
Alejandro F <afuste7@gmail.com>
2021-04-29 19:32:38 -0400

    text and span tag to TextField component

--- client/src/components/TextField/index.js
+++ client/src/components/TextField/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-function TextField(props) {
+function TextField({name}) {
     return (
         <div>
+            <h4>Hello, I'm <span>{name}</span></h4>
             
         </div>
     )
 }
 
 export default TextField
-props
