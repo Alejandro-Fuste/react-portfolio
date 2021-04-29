af75b4693141f5d76487154168d70a05bf07dd62
Alejandro F <afuste7@gmail.com>
2021-04-29 19:02:47 -0400

    removed commented out code

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-// import Title from '../components/ThreeText';
 import Box from '../components/Box';
 
 const Homepage = () => {
 	return (
 		<React.Fragment>
-			{/* <Title name="Developer" /> */}
+			
 			<Box />
 		</React.Fragment>
 	);
